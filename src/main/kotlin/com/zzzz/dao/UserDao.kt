9894e11d84c400b64bdf98e21fa547c52a587911
Zenas Chen <czyczk@qq.com>
2017-11-13 21:33:18 +0800

    Added comments

--- src/main/kotlin/com/zzzz/dao/UserDao.kt
+++ src/main/kotlin/com/zzzz/dao/UserDao.kt
@@ -3,18 +3,50 @@ package com.zzzz.dao
 import com.zzzz.enum.UserTypeEnum
 import com.zzzz.model.User
 import org.apache.ibatis.annotations.Param
+import org.springframework.dao.DuplicateKeyException
 
 interface UserDao {
+    /**
+     * Query a user by an ID
+     * @param id The ID of the user
+     * @return The required user or null if not found
+     */
     fun queryById(id: Long): User?
 
+    /**
+     * Query a user by a username
+     * @param username The username of the user
+     * @return The required user or null if not found
+     */
     fun queryByUsername(username: String): User?
 
+    /**
+     * Query users by a user type
+     * @param type The type of the users
+     * @return A list of users of that type
+     */
     fun queryByType(type: UserTypeEnum): List<User>
 
+    /**
+     * Insert a user
+     * @param username The username (should be unique)
+     * @param password The password
+     * @param type The user type
+     * @return Number of rows affected. 1 if succeeded or 0 if not (can be caused by a username conflict).
+     */
     fun insert(@Param("username") username: String,
                @Param("password") password: String,
                @Param("type") type: UserTypeEnum): Int
 
+    /**
+     * Update a user
+     * @param id The ID of the user to be updated
+     * @param username Specified if an update is needed
+     * @param password Specified if an update is needed
+     * @param type Specified if an update is needed
+     * @throws DuplicateKeyException Thrown when meeting a username conflict
+     */
+    @Throws(DuplicateKeyException::class)
     fun update(@Param("id") id: Long,
                @Param("username") username: String?,
                @Param("password") password: String?,
