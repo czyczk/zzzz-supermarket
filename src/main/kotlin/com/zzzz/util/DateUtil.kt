c229d74a660d66bd898efaf5db5ef0a11f5cd080
Zenas Chen <czyczk@qq.com>
2017-11-18 15:04:41 +0800

    new date utils

--- src/main/kotlin/com/zzzz/util/DateUtil.kt
+++ src/main/kotlin/com/zzzz/util/DateUtil.kt
@@ -1,12 +1,15 @@
 package com.zzzz.util
 
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.ZonedDateTime
+import java.time.*
 
 class DateUtil {
     companion object {
         fun toMilli(localDateTime: LocalDateTime): Long =
                 ZonedDateTime.of(localDateTime, ZoneId.systemDefault()).toInstant().toEpochMilli()
+
+        fun fromMilliToTime(epochMilli: Long): LocalDateTime =
+                ZonedDateTime.ofInstant(Instant.ofEpochMilli(epochMilli), ZoneId.systemDefault()).toLocalDateTime()
+
+        fun fromMilliToDate(epochMilli: Long): LocalDate = fromMilliToTime(epochMilli).toLocalDate()
     }
 }
