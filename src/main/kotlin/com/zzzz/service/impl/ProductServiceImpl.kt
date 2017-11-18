aaa4368e1609365108faea4d84e3401c31ab0d3d
Zenas Chen <czyczk@qq.com>
2017-11-18 15:34:21 +0800

    trivial

--- src/main/kotlin/com/zzzz/service/impl/ProductServiceImpl.kt
+++ src/main/kotlin/com/zzzz/service/impl/ProductServiceImpl.kt
@@ -10,6 +10,7 @@ import java.math.BigDecimal
 
 @Service
 class ProductServiceImpl : ProductService {
+    @Suppress("SpringKotlinAutowiring")
     @Autowired
     private lateinit var productDao: ProductDao
 
